093cc5dca7ed86fcdc8110a706438672ba7fd6d5
Kyle Welch <kwelch0626@gmail.com>
2017-02-08 22:34:42 -0600

    added script to create personal folders

---
+++ index.js
@@ -0,0 +1,54 @@
+import ghUser from 'github-current-user';
+import prompt from 'prompt';
+import chalk from 'chalk';
+import ncp from 'ncp';
+import path from 'path';
+
+console.log(chalk.blue('Creating user exercise folder...'));
+
+const getUsername = () => {
+  const username = ghUser.current((err, username) => {
+    if (err || !username) {
+      return 'unknown';
+    }
+    return username;
+  });
+  return username;
+};
+
+const createUserCopy = ({ username }) => {
+  ncp(path.resolve('./exercises'), username, function (err) {
+    if (err) {
+      console.log(chalk.red('Unable to create user specific copy.'));
+      return console.log(err);
+    }
+    console.log(chalk.blue(`Your personal copy of the exercises in a newly created folder(${username}).`));
+  });
+}
+
+const run = async() => {
+  const username = await getUsername();
+  const schema = {
+    properties: {
+      username: {
+        default: username,
+        pattern: /^[a-z0-9_-]{3,15}$/,
+        message: 'Name must be only letters, numbers, underscores, or dashes',
+      },
+    },
+  };
+
+  prompt.start();
+
+  prompt.get(schema, (err, results) => {
+    if (err) {
+      console.log(chalk.red('Failed to determine folder to create.'));
+      console.log(err);
+      process.exit(1);
+      return 1;
+    }
+    createUserCopy(results);
+  });
+};
+
+run();
